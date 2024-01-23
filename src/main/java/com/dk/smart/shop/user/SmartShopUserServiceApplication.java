dc6cfba06e08ce424d21ab46b685db28afcb26e2
Durga-Kalyan17 <durgakalyan017@gmail.com>
2024-01-23 16:21:35 +0530

    first commit

---
+++ src/main/java/com/dk/smart/shop/user/SmartShopUserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dk.smart.shop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SmartShopUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShopUserServiceApplication.class, args);
+	}
+
+}
